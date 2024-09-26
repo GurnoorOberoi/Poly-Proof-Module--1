fa88397567983e21a819aa4f55eb4b0cc809a20e
GurnoorOberoi <gurnoor9965@gmail.com>
2024-09-27 03:51:12 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -6,34 +6,45 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-
-const tokenAddress = ""; // place your erc20 contract address here
+const tokenContractJSON = require("../artifacts/contracts/rawrNFT.sol/rawrNFT.json");
+const NUMBER_OF_TOKENS = 5;
+const tokenAddress = "0xE153Cbba8732236c3b5e58120731f12ec84f9260"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
-const fxERC20RootAddress = "0x3658ccFDE5e9629b0805EB06AaCFc42416850961";
-const walletAddress = ""; // place your public address for your wallet here
+const fxERC721RootTunnel = "0x9E688939Cb5d484e401933D850207D6750852053";
+const walletAddress = "0x9BD5EE362566B0eB960fD9Ad7975119B09CD9264"; // place your public address for your wallet here
 
 async function main() {
+  const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+  const fxContract = await hre.ethers.getContractAt(
+    fxRootContractABI,
+    fxERC721RootTunnel
+  );
 
-    const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-    const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC20RootAddress);
+  const approveTx = await tokenContract.setApprovalForAll(
+    fxERC721RootTunnel,
+    true
+  );
+  await approveTx.wait();
 
-    const approveTx = await tokenContract.approve(fxERC20RootAddress, 500);
-    await approveTx.wait();
+  console.log("Approval confirmed");
 
-    console.log('Approval confirmed');
-
-
-    const depositTx = await fxContract.deposit(tokenAddress, walletAddress, 500, "0x6556");
+  for (let i = 0; i < NUMBER_OF_TOKENS; i++) {
+    const depositTx = await fxContract.deposit(
+      tokenAddress,
+      walletAddress,
+      i,
+      "0x6556"
+    );
     await depositTx.wait();
-
-    console.log("Tokens deposited");
-  
+    console.log(`Depositing ${i+1}: Successful`);
   }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+
+  console.log("Tokens deposited");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
