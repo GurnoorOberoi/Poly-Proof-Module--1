e6336f038e7564349b0994b20e455c4cd63dbf22
GurnoorOberoi <gurnoor9965@gmail.com>
2024-09-28 20:24:56 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -5,19 +5,35 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/rawrNFT.sol/rawrNFT.json");
+const tokenContractJSON = require("../artifacts/contracts/rawNFT.sol/rawrNFT.json");
 
-const tokenAddress = "0xfc9d8bf1c28289E1392271916CBFb14Bca13455a"; // place your erc20 contract address here
+const tokenAddress = "0xB4043e884D87c92BEE167b54C4613BEa244652E3"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0x9BD5EE362566B0eB960fD9Ad7975119B09CD9264"; // place your public address for your wallet here
-
+const walletAddress = "0x6601a5fD74929235Cd69e501439d5B0BAd4E7363"; // place your public address for your wallet here
 async function main() {
+  const imageUrls = [
+    "https://bafybeihfa4kxlbq7rq364qi76kq3xpg4ot2fpzk6qxpgqc7wboji2atgry.ipfs.dweb.link/",
+    "https://bafybeiep5f5nwruxsmbi4pgiizdm7t4u222uy26yg7o4ds2zm5syptwdsy.ipfs.dweb.link/",
+    "https://bafybeibyr5tc4qkvftb47cpiicfwyxnerxlujhdrcuf4orkzd7gblw3ooe.ipfs.dweb.link/",
+    "https://bafybeif272oslaqvdjma4c7nk3p4nnq2gsjkdcoufgpkp74skl7g3w4c24.ipfs.dweb.link/",
+    "https://bafybeibffxqcgdel6ca5p637q5pkfdwv7boodr64bc57dp6cgr77g5ihy4.ipfs.dweb.link/",
+  ];
 
-    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+  const prompts = [
+    "imagine an image of super snake with 7 heads. Make it a deity with super powers",
+    "imagine an image of an eagle soaring through the sky, with magical super power aura around it",
+    "imagine an image of a ninja in shadowy background with a shiny and reflecting blade",
+    "imagine an image of earth. Make the earth wear goggles driving a kart",
+    "imagine an image of a cat as a superman with glowing eyes and super energy beam with super power aura",
+  ];
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
+  const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+
+  const tx = await token.mintNft(imageUrls, prompts);
+  await tx.wait();
+
+  console.log(`You now have minted ${imageUrls.length} NFTs`);
+}
   // We recommend this pattern to be able to use async/await everywhere
   // and properly handle errors.
   main().catch((error) => {
